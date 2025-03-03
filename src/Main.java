efa7b1bd19ff365e37183894515aaaa70ef033e0
hleb <hlebp@nycstudents.net>
2025-03-03 10:44:06 -0500

    Spacing changes

--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,13 @@ public class Main {
         int totalInt = 0 ;
         Rectangle first = new Rectangle(20,10,10,15);
 
+
         for(int i = 0 ; i < 1000000; i ++){
             int randomLength = (int) (Math.random() * 40) + 11;
             int randomWidth = (int) (Math.random() * 5) +6;
             int randomX =  (int) (Math.random() * 50) +1;
             int randomY = (int) (Math.random() * 50) +1;
+
 
             Rectangle newRectangle = new Rectangle(randomLength,randomWidth,randomX,randomY);
 
--- src/Rectangle.java
+++ src/Rectangle.java
@@ -17,8 +17,6 @@ public class Rectangle {
             touches = true;
         }
 
-
-
     }
 
 
