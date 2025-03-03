2e5b8dbe85095a323b66ffa3a74f4aef56554518
hleb <hlebp@nycstudents.net>
2025-03-03 09:51:01 -0500

    Rectangle class initiated

---
+++ src/Rectangle.java
@@ -0,0 +1,20 @@
+public class Rectangle {
+    private int length;
+    private int width;
+    private int x;
+    private int y;
+
+    public Rectangle(int length, int width, int x, int y){
+        this.length = length;
+        this.width = width;
+        this.x = x;
+        this.y = y ;
+
+
+
+    }
+
+
+
+
+}
